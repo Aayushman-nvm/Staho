54dd15c0acccfbb7f081624c384328d7e5823c65
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-11-02 21:47:51 +0530

    Styled heading

--- app/components/ui/Heading.tsx
+++ app/components/ui/Heading.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-interface Heading{
-    title:string,
-    subTitle?:string,
-    center?:boolean,
+interface HeadingProps {
+  title: string;
+  subTitle?: string;
+  center?: boolean;
 }
 
-function Heading({title, subTitle,center}:Heading) {
+function Heading({ title, subTitle, center }: HeadingProps) {
   return (
-    <div>Heading
-        {title}
-        {subTitle}
+    <div className={center ? "text-center" : "text-start"}>
+      <h2 className="text-2xl font-bold">{title}</h2>
+      {subTitle && (
+        <p className="font-light text-neutral-500 mt-2">{subTitle}</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default Heading
+export default Heading;
