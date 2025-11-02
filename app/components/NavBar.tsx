97e6efa142c2ecfcf46401ac9aa9581ae8cf86ec
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-11-02 21:01:17 +0530

    Unstyled navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Search from "./ui/Search"
+import UserMenu from "./ui/UserMenu"
+
+function NavBar() {
+  return (
+    <div>NavBar
+        <Search/>
+        <UserMenu/>
+    </div>
+  )
+}
+
+export default NavBar
