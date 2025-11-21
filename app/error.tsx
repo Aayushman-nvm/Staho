42e5741defd219e6c664878ee6b1921b25ff4809
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-11-22 00:05:07 +0530

    Error page

---
+++ app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect } from "react";
+import EmptyState from "./components/EmptyState";
+
+interface ErrorStateProps {
+  error: Error;
+}
+
+function error({ error }: ErrorStateProps) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return <EmptyState title="Uh Oh" subtitle="Something went wrong!" />;
+}
+
+export default error;
