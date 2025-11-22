9ff9b9c2bd6d6bc600e1a8b7a5bf7929d2b16de8
Aayushman-nvm <aayushman.jha001@gmail.com>
2025-11-22 12:18:29 +0530

    route error fixed

--- app/api/reservations/[reservationId]/route.ts
+++ app/api/reservations/[reservationId]/route.ts
@@ -3,31 +3,33 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
 
 interface IParams {
-  reservationId?: string;
+  reservationId: string;
 }
 
 export async function DELETE(
   request: Request,
-  { params }: { params: IParams }
+  { params }: { params: Promise<IParams> }
 ) {
   const currentUser = await getCurrentUser();
   if (!currentUser) {
     return NextResponse.error();
   }
 
-  const { reservationId } = params;
-  if (!reservationId || typeof reservationId != "string") {
+  const { reservationId } = await params;
+
+  if (!reservationId || typeof reservationId !== "string") {
     throw new Error("Invalid Id");
   }
 
   const reservation = await prisma.reservation.deleteMany({
-    where:{
-        id: reservationId,
-        OR:[
-            {userId: currentUser.id},
-            {listing: {userId: currentUser.id}}
-        ]
-    }
+    where: {
+      id: reservationId,
+      OR: [
+        { userId: currentUser.id },
+        { listing: { userId: currentUser.id } },
+      ],
+    },
   });
+
   return NextResponse.json(reservation);
 }
